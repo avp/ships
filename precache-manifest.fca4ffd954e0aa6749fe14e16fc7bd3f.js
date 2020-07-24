fcba26136d8d32c456bf5ce7f74494ffc1140348
avp <aakpat6@gmail.com>
2020-07-24 03:06:50 +0000

    Deploying to gh-pages from  @ cf5eb82712b4971f7f9c90a02bec41a56d442089 🚀

---
+++ precache-manifest.fca4ffd954e0aa6749fe14e16fc7bd3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79ebfcb333fbbed925ff047fd73d857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ced6cc6e6f1ea72b7df5",
+    "url": "/static/css/main.73324ec4.chunk.css"
+  },
+  {
+    "revision": "6ebfc469b1a7b2a17f68",
+    "url": "/static/js/2.a9b07d53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9b07d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced6cc6e6f1ea72b7df5",
+    "url": "/static/js/main.d42adbb8.chunk.js"
+  },
+  {
+    "revision": "d14ce106c3c08db960ee",
+    "url": "/static/js/runtime-main.b1beee5b.js"
+  }
+]);
